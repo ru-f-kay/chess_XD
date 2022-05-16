333cd76ca5440e46e58cd4bda6f712a13056ddbf
A. F. Kay <afkay.dev@yandex.ru>
2022-05-17 01:04:21 +0300

    Add chess figures info

---
+++ src/const/index.ts
@@ -0,0 +1,10 @@
+import { FigureInfo, FigureType } from "../types";
+
+export const FIGURES_INFO: Record<FigureType, FigureInfo> = {
+  [FigureType.King]: { name: 'King' },
+  [FigureType.Queen]: { name: 'Queen' },
+  [FigureType.Rook]: { name: 'Rook' },
+  [FigureType.Bishop]: { name: 'Bishop' },
+  [FigureType.Knight]: { name: 'Knight' },
+  [FigureType.Pawn]: { name: 'Pawn' },
+};
