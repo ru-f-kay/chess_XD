e6a415521ccc6e1953bace94f1f1f7ac1919c84e
A. F. Kay <afkay.dev@yandex.ru>
2022-05-17 01:03:58 +0300

    Add chess figures info

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export enum FigureType {
+  King = "K",
+  Queen = "Q",
+  Rook = "R",
+  Bishop = "B",
+  Knight = "N",
+  Pawn = "P",
+}
+
+export type FigureInfo = {
+  name: string;
+}
+
+export enum FigureColor {
+  Black = "b",
+  White = "w",
+}
