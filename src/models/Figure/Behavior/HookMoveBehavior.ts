9b268f4b0a7158414b401332343606793de2f043
A. F. Kay <afkay.dev@yandex.ru>
2022-05-18 04:44:01 +0300

    Add Hook move behavior

--- src/models/Figure/Behavior/HookMoveBehavior.ts
+++ src/models/Figure/Behavior/HookMoveBehavior.ts
@@ -1,10 +1,26 @@
 import { TurnResult } from "../../../types/game";
 import { Board } from "../../Board";
 import { Figure } from "../Figure";
+import { getTurnResultInOffset } from "./helper";
 import { IMoveBehavior } from "./IMoveBehavior";
 
 export class HookMoveBehavior implements IMoveBehavior {
   getAvailableMoves = (figure: Figure, board: Board): TurnResult[] => {
-    throw new Error('Hook move: Not implemented');
+    const hookMoveOffsets: [number, number][] = [
+      [-2, +1], [-2, -1],
+      [+2, -1], [+2, +1],
+      [-1, -2], [+1, -2],
+      [-1, +2], [+1, +2],
+    ]
+
+    // checked for null but TS compiler is not smart enough :( But I still love it <3
+    // @ts-ignore
+    return hookMoveOffsets.map(
+      offset => getTurnResultInOffset(
+        board,
+        { ...figure.position, color: figure.color },
+        offset
+      )
+    ).filter(Boolean);
   }
 }
