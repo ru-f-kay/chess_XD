ebead99b13600ac77719ccef9dcd0eb4d707bdb4
A. F. Kay <afkay.dev@yandex.ru>
2022-05-18 04:29:39 +0300

    Add pawn moves logic

--- src/models/Figure/Behavior/PawnMoveBehavior.ts
+++ src/models/Figure/Behavior/PawnMoveBehavior.ts
@@ -1,10 +1,39 @@
-import { TurnResult } from "../../../types/game";
+import { filterNotEmpty } from "../../../utils/common";
+import { MoveDirection } from "../../../types/figures";
+import { GameColor, TurnResult } from "../../../types/game";
 import { Board } from "../../Board";
 import { Figure } from "../Figure";
+import { getMovesInDirection, getTurnResultInOffset } from "./helper";
 import { IMoveBehavior } from "./IMoveBehavior";
 
 export class PawnMoveBehavior implements IMoveBehavior {
   getAvailableMoves = (figure: Figure, board: Board): TurnResult[] => {
-    throw new Error('Pawn move: Not implemented');
+    const maxStepsForward = figure.turnsMade === 0 ? 2 : 1;
+    const direction = figure.color === GameColor.White ? MoveDirection.Up : MoveDirection.Down;
+
+    const resultsForward = getMovesInDirection({
+      board,
+      figure,
+      direction,
+      steps: maxStepsForward,
+    });
+
+    const killOffsetPairs: [number, number][] = direction === MoveDirection.Up ? [
+      [-1, -1],
+      [-1, +1],
+    ] : [
+      [+1, -1],
+      [+1, +1],
+    ];
+
+    // TS compiler can't believe :(
+    // @ts-ignore
+    const resultsKill: TurnResult[] = killOffsetPairs.map(offsets => getTurnResultInOffset(
+      board,
+      { ...figure.position, color: figure.color },
+      offsets
+    )).filter(Boolean);
+
+    return [...resultsForward, ...resultsKill];
   }
 }
